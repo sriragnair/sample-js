b05292e7ae8ada6d705c10dbb112d718b4d398ae
Srirag Nair <ksriragnair@gmail.com>
2023-12-19 21:12:09 -0800

    Fix test

--- app.test.js
+++ app.test.js
@@ -1,11 +1,21 @@
-const app = require('./app'); // Adjust this line if your express app is defined in a different file
+const app = require('./app');
 const supertest = require('supertest');
 const request = supertest(app);
+let server;
 
-it('gets the test endpoint', async done => {
+// Start server before all tests
+beforeAll((done) => {
+  server = app.listen(4000, done); // Choose a test port
+});
+
+// Close server after all tests
+afterAll((done) => {
+  server.close(done);
+});
+
+it('gets the test endpoint', async () => {
   const response = await request.get('/');
 
   expect(response.status).toBe(200);
   expect(response.text).toBe('Hello World!');
-  done();
 });
